42d85dd5b9dc6d2d6f708e02299e433738976888
LukaszB <lukaszbugajski1998@@gmail.com>
2020-02-03 22:13:55 +0100

    daro pobieraj

--- skr.js
+++ skr.js
@@ -2,3 +2,11 @@ for(let i=1;i<=10;i++){
 
     console.log(i);
 }
+
+console.log("Zajebiscie bo se moge srednika zapomniec i tablice robic z roznymi typami")
+
+let n = [1,'komputer jak komputer',null,true];
+
+console.log("W c# musialbym zrobic petle zeby wyswietlic tablice xDDDDD")
+
+console.log(n);
