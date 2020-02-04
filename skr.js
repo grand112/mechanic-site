c7a0e44f706877496d49dd61d4c5ea2e489dcbd7
LukaszB <lukaszbugajski1998@@gmail.com>
2020-02-05 00:03:45 +0100

    topka

--- skr.js
+++ skr.js
@@ -1,6 +1,15 @@
-let napis = document.querySelector('.top');
-document.querySelector('.btn').addEventListener('click', function(){
-    napis.innerHTML="we mnie nie klikaj";
-});
+let navChild = document.querySelectorAll('.link');
+
+for(let i=0;i<navChild.length;i++)
+{
+    navChild[i].addEventListener("mouseover", function (){
+        navChild[i].style.color="white"
+        navChild[i].style.textDecoration="none";
+    });
+
+    navChild[i].addEventListener("mouseleave", function (){
+        navChild[i].style.color="grey"
+    });
+}
 
 
