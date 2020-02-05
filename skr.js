85d672bac341a10f18870415ef60ce014e72917d
LukaszB <lukaszbugajski1998@@gmail.com>
2020-02-05 16:57:53 +0100

    z jsa mid

--- skr.js
+++ skr.js
@@ -3,13 +3,21 @@ let navChild = document.querySelectorAll('.link');
 for(let i=0;i<navChild.length;i++)
 {
     navChild[i].addEventListener("mouseover", function (){
-        navChild[i].style.color="white"
+        navChild[i].style.color="white";
         navChild[i].style.textDecoration="none";
     });
 
     navChild[i].addEventListener("mouseleave", function (){
-        navChild[i].style.color="grey"
+        navChild[i].style.color="grey";
     });
 }
+
+let tryMe = document.querySelector('.btn');
+let mid = document.querySelector('.mid');
+
+tryMe.addEventListener('click', function(){
+    mid.innerHTML=`<h2>To gowno zostalo wygenerowane z jsa</h2>`;
+});
+
 
 
