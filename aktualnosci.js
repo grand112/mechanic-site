483b3a288a9b42584e23fa2871bbb8660343f115
LukaszB <lukaszbugajski1998@@gmail.com>
2020-02-05 20:41:04 +0100

    prawie artykuly

--- aktualnosci.js
+++ aktualnosci.js
@@ -2,27 +2,43 @@ let akt = document.querySelector('.mid');
 let date = new Date()
 let title = "Wybrano pracownika miesiąca"
 let tresc = "To powinno być skądś pobierane a nie, że se piszemy tak o w js wpisy. Nwm jescze jak to zrobić. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-console.log(date.getDay());
-console.log(date);
-(function () {
-    akt.innerHTML = `
-                        <div class="akt container">
-                            <div class="wpis">
-                                <div class="foto>
-                                    <img src="http://m7.mech.pk.edu.pl/images/stories/p_osocha.jpg">
+let place = "Kraków";
+let content = `
+                    <div class="akt container px-4 py-4 my-4">
+                        <div class="wpis row">
+                            <div class="foto col-md-4 px-5" >
+                                <img class ="doWpisu" src="http://m7.mech.pk.edu.pl/images/stories/p_osocha.jpg">
+                            </div>
+                            <div class="opis col-md-8 px-2 py-4 ">
+                                <div class ="title h5 font-weight-bold text-center">
+                                    ${title}
                                 </div>
-                                <div class="data">
-                                    ${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}r. 
+                                <div class ="tresc px-3 py-3 text-justify">
+                                    <hr>
+                                    ${tresc}
+                                    <hr>
+                                    <button type="button" class="btn btn-secondary float-right">czytaj więcej (nie dziala narazie)</button>
                                 </div>
-                                <div class="opis">
-                                    <div class ="title">
-                                        ${title}
-                                    </div>
-                                    <div class ="tresc">
-                                        ${tresc}
-                                    </div>
+                                <div class="data px-3">
+                                    <hr>
+                                    ${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}r. <br>
+                                    ${place}
                                 </div>
                             </div>
                         </div>
-                    `;
+                    </div>
+                    <div class="wiecej float-right mx-3">
+                    <button type="button" class="btn btn-dark">Wyświetl wiecej</button>
+                    </div>
+                `;
+
+(function () {
+    akt.innerHTML = content;
 })();
+
+let wiecej = document.querySelector('.wiecej');
+wiecej.addEventListener("click", function(){
+    console.log('elo');
+    content =+content;
+    akt.innerHTML = content;
+});
