ffe26c20c0cb485791a926515c23b00d2eca9970
LukaszB <lukaszbugajski1998@@gmail.com>
2020-02-06 15:45:50 +0100

    zmiana

--- skr.js
+++ skr.js
@@ -1,5 +1,5 @@
+// po najechaniu na element menu sie podswietla i gasnie
 let navChild = document.querySelectorAll('.link');
-
 for(let i=0;i<navChild.length;i++)
 {
     navChild[i].addEventListener("mouseover", function (){
@@ -12,19 +12,21 @@ for(let i=0;i<navChild.length;i++)
     });
 }
 
-let home = document.querySelector(".home");
-home.addEventListener("click", rel);
-function rel(){
+// przeladowanie strony po nacisnieciu logo lub opcji home w menu
+let home = document.querySelectorAll(".home");
+for(let i=0;i<home.length;i++)
+{
+    home[i].addEventListener("click", rel);
+    function rel(){
     window.location.reload(true);
+    }
 }
 
-
+// po wybaniu opcji kontakt lub oNas w menu strona przekierwoywuje nas do stopki
 let kontakt = document.querySelector('.kontakt');
 let oNas = document.querySelector('.oNas');
-
 kontakt.addEventListener("click", toBottom);
 oNas.addEventListener("click", toBottom);
-
 function toBottom(){
     window.scrollTo(0,document.body.scrollHeight);
 }
