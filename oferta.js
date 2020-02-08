dd113d0bcff404830902c0f1adadeeff1a2ff2f1
hezyrof1997 <hezyrof@gmail.com>
2020-02-08 17:33:53 +0100

    poprawka oferty

--- oferta.js
+++ oferta.js
@@ -1,11 +1,11 @@
 //zmienne do przetrzymywania tekstów na karty
 
 let tekst1=`
-Diagnostyka współczesnego samochodu polega na podłączeniu go do specjalnego komputera diagnostycznego do komputera pokładowego pojazdu w celu wymiany danych pomiędzy nimi. Tester diagnostyczny w ciągu zaledwie kilku minut odczytuje zapisane przez komputer pokładowy informacje o stanie auta i wyświetla te, które dotyczą wykrytych nieprawidłowości w funkcjonowaniu poszczególnych podzespołów. W ten sposób specjalista będzie mógł rozpoznać problem, a następnie rozpocząć jego usuwanie.
+Diagnostyka współczesnego samochodu polega na podłączeniu go do specjalnego komputera diagnostycznego do komputera pokładowego pojazdu w celu wymiany danych pomiędzy nimi. Tester diagnostyczny w ciągu zaledwie kilku minut odczytuje zapisane przez komputer pokładowy informacje o stanie auta i wyświetla te, które dotyczą wykrytych nieprawidłowości w funkcjonowaniu poszczególnych podzespołów. 
 `;
 
 let tekst2=`
-Postępujemy zgodnie z zaleceniami producentów w zakresie wymiany części narażonych na eksploatację, chyba że element został uszkodzony przed terminem przewidywanej przez producenta wymiany. W takiej sytuacji wprowadzamy do pojazdu nowe, sprawne części. Przeprowadzana jest diagnostyka oraz naprawa nowoczesnych układów hamulcowych, uwzględniająca także działania w zakresie elektrycznych hamulców postojowych. Ponadto eksperci podpowiedzą jak uniknąć awarii, wynikających z nieprawidłowej eksploatacji pojazdu.
+Postępujemy zgodnie z zaleceniami producentów w zakresie wymiany części narażonych na eksploatację, chyba że element został uszkodzony przed terminem przewidywanej przez producenta wymiany. W takiej sytuacji wprowadzamy do pojazdu nowe, sprawne części. Przeprowadzana jest diagnostyka oraz naprawa nowoczesnych układów hamulcowych, uwzględniająca także działania w zakresie elektrycznych hamulców postojowych.
 `
 let tekst3=`
 Typowe bolączki niesprawnego układu zapłonowego w pojazdach z zapłonem iskrowym to problemy z rozruchem, szarpanie, niekiedy falujące obroty i nierówna praca silnika. Zapobieganie awariom układu zapłonowego zwykle ogranicza się do stosowania dobrej jakości paliwa, jak też regularnej wymiany niektórych elementów: świec oraz - dawniej - przewodów zapłonowych, kopułki i tzw. palca rozdzielacza aparatu zapłonowego.
@@ -40,10 +40,10 @@ Oferta
 </div>
 
         <div class="container d-flex justify-content-around godz">
-            <div class="row" >
+            <div class="row justify-content-center" >
 
-                <div class="col">
-                    <div class="karta text-white card bg-secondary mb-3" style="max-width: 18rem;">
+                <div class="col-lg-4 col-sm-12 col-md-6">
+                    <div class="karta text-white card bg-secondary mb-1 mx-5" style="max-width: 18rem;">
                         <div class="card-header">${naglowki[0]}</div>
                         <div class="card-body">
                             <p class="card-text">${czesci[0]}</p>
@@ -51,8 +51,8 @@ Oferta
                     </div>
                 </div>
 
-                <div class="col">
-                    <div class="karta text-white card bg-secondary mb-3" style="max-width: 18rem;">
+                <div class="col-lg-4 col-sm-12  col-md-6">
+                    <div class="karta text-white card bg-secondary mb-1 mx-5" style="max-width: 18rem;">
                         <div class="card-header">${naglowki[1]}</div>
                         <div class="card-body">
                             <p class="card-text">${czesci[1]}</p>
@@ -60,8 +60,8 @@ Oferta
                     </div>
                 </div>
 
-                <div class="col">
-                    <div class="karta text-white card bg-secondary mb-3" style="max-width: 18rem;">
+                <div class="col-lg-4 col-sm-12  col-md-6">
+                    <div class="karta text-white card bg-secondary mb-1 mx-5" style="max-width: 18rem;">
                         <div class="card-header">${naglowki[2]}</div>
                         <div class="card-body">
                             <p class="card-text">${czesci[2]}</p>
@@ -95,5 +95,9 @@ for(let i=0;i<karta.length;i++)
         </div>`
     });
 }
+
+
+mid.scrollIntoView();
+
 }
 );
